7811eb98e6fc590d1a33944dbd366a2758b68649
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 15:01:40 +0300

    Add Object pairing value keys with roman values

--- 02_romanNumeralConverter.js
+++ 02_romanNumeralConverter.js
@@ -1,6 +1,21 @@
 // Roman Numeral Converter
 // Convert the given number into a roman numeral
 
+const romanNumerals = {
+    1000: 'M',
+    900: 'CM',
+    500: 'D',
+    400: 'CD',
+    100: 'C',
+    90: 'XC',
+    50: 'L',
+    40: 'XL',
+    10: 'X',
+    9: 'IX',
+    5: 'V',
+    1: 'I'
+};
+
 function convertToRoman(num) {
     return num;
 }
