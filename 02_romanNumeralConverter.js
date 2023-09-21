207ea02aff378d589b0d543e418c106329dbcc54
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-21 10:29:52 +0300

    Add exp variable to hold the largest factor of 10

--- 02_romanNumeralConverter.js
+++ 02_romanNumeralConverter.js
@@ -22,17 +22,17 @@ const romanNumerals = {
  * @returns a roman numeral equivalent of the num passed to the function
  */
 function convertToRoman(num) {
-    // The num passed in will become an equivalent romanNumeral string
+    // 1. The num passed in will become an equivalent romanNumeral string
     let romanNumeral = ""; 
 
+    // 2. Get the largest factor of 10 for num
+    let exp = String(num).length - 1;
+    console.log(`Magnitude of ${num} is ${exp}`)
+    
     // Iterate through the romanNumerals object and print keys to console.
     for (const numeral in romanNumerals) {
         console.log(numeral);
     }
-
-    // get the magnitude of num
-    const mag = String(num).length - 1;
-    console.log(`Magnitude of ${num} is ${mag}`)
 
     return num;
 }
