2effbe996643ee39eba27bdd2f99976f8e2fd74e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-29 17:02:21 +0300

    Match leading 1 + () + - separator

--- 04_telephoneNumberValidator.js
+++ 04_telephoneNumberValidator.js
@@ -34,7 +34,8 @@ function telephoneCheck(str) {
     // const regex = /^\(\d{3}\)\s?\d{3}\-\d{4}|^(1\s)?\d{3}(\s?\-?)\d{3}\2\d{4}$/; // match only 10-digits strings when there is a leading 1 
 
     // const regex = /^1(\s)\d{3}\1\d{3}\1\d{4}$/g;  // match leading 1 with spaces delimiting
-    const regex = /^1(\s)\(\d{3}\)\1\d{3}\-\d{4}$/g;  // match parentheses around first 3-digit group and a hyphen separating final groups
+    // const regex = /^1(\s)\(\d{3}\)\1\d{3}\-\d{4}$/g;  // match parentheses around first 3-digit group and a hyphen separating final groups
+    const regex = /^1\(\d{3}\)\d{3}\-\d{4}$/g;  // match leading 1 with parentheses around first 3-digits succeeding 1 and a hyphen separating final groups
 
 
     // Combining the above regexes with the or operator gets more matches
