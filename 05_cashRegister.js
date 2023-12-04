e3a0754ab4f61c4bcf3884278ab3f1c65eb13afa
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-12-04 21:20:34 +0200

    Refactor and annotate code

--- 05_cashRegister.js
+++ 05_cashRegister.js
@@ -45,11 +45,26 @@ See below for an example of a cash-in-drawer array:
   ["ONE HUNDRED", 100]
 ]
 */
-function hasFunds(coinName, value, cid) {
+/**
+ * Updates the cash in drawer array
+ * @param {*} coinName 
+ * @param {*} coinValue 
+ * @param {*} cid 
+ */
+function deductFunds(coinName, coinValue, cid) {
   for (let i = 0; i < cid.length; i++) {
     const element = cid[i];
-    if (element[0] == coinName && element[1] >= value) {
-      console.log(element[0], element[1]);
+    if (element[0] == coinName) {
+      element[1] -= coinValue.toFixed(2);
+    }
+  }
+}
+
+function hasFunds(coinName, coinValue, cid) {
+  for (let i = 0; i < cid.length; i++) {
+    const element = cid[i];
+    if (element[0] == coinName && element[1] >= coinValue) {
+      console.log(element[0], element[1], true);
       return true;
     }
   }
@@ -57,50 +72,66 @@ function hasFunds(coinName, value, cid) {
 }
 
 function checkCashRegister(price, cash, cid) {
-  let change = cash - price;
+  console.log(typeof(price), typeof(cash), typeof(cid));
+
+  // Calculate changeDue and limit result to two decimal places.
+  let changeDue = (cash - price);
+  console.log("cash: ", cash.toFixed(2), 
+  "\nprice: ", price.toFixed(2), 
+  "\nchangeDue: ", changeDue.toFixed(2));
+
+  // Get the total value of cid
+  const drawerValue = cid.reduce((sum, element) => sum += element[1], 0);
+  console.log("\ndrawerValue: ", drawerValue.toFixed(2));
 
   // determine how to give change
-  let coins = [
-    ["QUARTER", 0.25],
-    ["DIME", 0.10],
-    ["NICKEL", 0.05],
-    ["PENNY", 0.01]
-  ];
-
-  let balance = change;
+  let balance = changeDue;
   let makeChange = [];
 
-  for (let i = 0; i < coins.length; i++) {
-    const coin = coins[i];
-    const coinName = coin[0];
-    const coinValue = coin[1];
+  // for (let i = 0; i < coins.length; i++) {
+  //   const coin = coins[i];
+  //   const coinName = coin[0];
+  //   const coinValue = coin[1];
     
-    let coinCount = 0;
-    console.log(hasFunds(coinName, balance, cid));
-    while (balance > coinValue) {
-      balance -= coinValue;
-      coinCount += 1;
-    }
-    if (balance == coinValue) {
-        balance -= coinValue;
-        coinCount += 1;
-    }
-    if (coinValue * coinCount > 0) {
-      makeChange.push([coinName, coinValue * coinCount]);
-    }
-  }
+  //   let coinCount = 0;
+  //   console.log("Balance is: " + balance);
+  //   while (balance >= coinValue && hasFunds(coinName, coinValue, cid)) {
+  //     balance -= coinValue.toFixed(2);
+  //     deductFunds(coinName, coinValue, cid)
+  //     coinCount += 1;
+          
+  //     if (balance.toFixed(2) == coinValue && hasFunds(coinName, coinValue, cid)) {
+  //       balance -= coinValue.toFixed(2);
+  //       deductFunds(coinName, coinValue, cid)
+  //       coinCount += 1;
+  //     }
+  //   }
 
-  return change;
+  //   if (coinValue * coinCount > 0) {
+  //     makeChange.push([coinName, coinValue * coinCount]);
+  //   }
+  // }
+
+  for (const denomination of makeChange) {
+    console.log(denomination);
+  }  
+  
+  if (cid < changeDue || !canReturnExactChange) {
+    return {status: "INSUFFICIENT_FUNDS", change: []}
+  } else if (cid == changeDue) {
+    return {status: "CLOSED", change: [...cid]}
+  }
+  return {"status": "OPEN", "change": [...cid]};
 }
 
 const testData = [
     [  19.5, 20, 
       [["PENNY", 1.01], ["NICKEL", 2.05],
-        ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55],
+        ["DIME", 3.1], ["QUARTER", 0.25], ["ONE", 90], ["FIVE", 55],
         ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]
     ],
 ];
 
-checkCashRegister(testData[0][0], testData[0][1], testData[0][2]);
+console.log(checkCashRegister(testData[0][0], testData[0][1], testData[0][2]));
 
 // {status: "OPEN", change: [["QUARTER", 0.5]]}
